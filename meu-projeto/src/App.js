a518d6d9e1349a13087fc92e2aff900c6d339ac6
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2024-07-23 16:23:49 -0300

    Primeiros passos

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu primeiro programa em React Native</h1>
+      <p>Isso e REACT</p>
+    </div>
+  );
+}
+
+export default App;
